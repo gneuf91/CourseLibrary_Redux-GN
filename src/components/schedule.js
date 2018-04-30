b2f46a22df9dbede4d26110c4d6b9dcb763dec41
Gaven Neufeld <gneuf91@gmail.com>
2018-04-30 15:15:25 -0600

    generate up to 5 empty slots

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -8,15 +8,42 @@ class Schedule extends Component {
         super(props) 
 
         this.renderCourse = this.renderCourse.bind(this);
+
+        this.state = {
+            enrolled: []
+        }
     }
 
     renderCourse(course) {
         return (
-            <div key={this.props.courses.indexOf(course)} className={`slot ${course.enrolled ? 'slot__course' : 'slot__empty'}`}>
+            <div key={this.state.enrolled.indexOf(course)} className={`slot ${course.enrolled ? 'slot__course' : 'slot__empty'}`}>
                 <div>{course.enrolled ? course.title : 'Empty Slot'}</div>
                 <a className={`action slot__remove`} onClick={() => this.props.removeCourse(course)}>remove course</a>
             </div>
         )
+    }
+
+    componentWillReceiveProps(nextProps) {
+        var newEnrolled = []
+
+        this.state.enrolled.map((course) => {
+            if(course.enrolled) {
+                console.log(course.title);
+                newEnrolled.push(course);
+            }
+        })
+        nextProps.courses.map((course) => {
+            if(course.enrolled && !newEnrolled.includes(course)) {
+                console.log(course.title);
+                newEnrolled.push(course)
+            }
+        })
+        for(var i = newEnrolled.length; i < 5; i++) {
+            newEnrolled.push({enrolled: false})
+        }
+        this.setState({
+            enrolled: newEnrolled
+        })
     }
 
     render() {
@@ -24,7 +51,7 @@ class Schedule extends Component {
             <div>
                 <div className="schedule__slots">
                     {
-                        this.props.courses.map(this.renderCourse)
+                        this.state.enrolled.map(this.renderCourse)
                     }
                 </div>
             </div>
@@ -35,7 +62,13 @@ class Schedule extends Component {
 }
 
 function mapStateToProps(state) {
-    return { courses: state.courses };
+    var enrolledCourses = []
+    state.courses.map((course) => {
+        if(course.enrolled) {
+            enrolledCourses.push(course);
+        }
+    })
+    return { courses: enrolledCourses };
 }
 
 function mapDispatchToProps(dispatch) {
