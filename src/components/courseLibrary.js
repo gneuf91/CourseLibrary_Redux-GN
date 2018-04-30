4b19ed1d214b23ba764c39c73a8a664073fbc652
Gaven Neufeld <gneuf91@gmail.com>
2018-04-30 14:13:31 -0600

    added styles to added courses section

--- src/components/courseLibrary.js
+++ src/components/courseLibrary.js
@@ -24,7 +24,7 @@ class CourseLibrary extends Component {
                 </div>
             <a className={`course__arrow ${course.open ? null : 'course__arrow-close'}`} onClick={() => this.props.toggleDescription(course)}></a>
             <a className={`action ${course.enrolled ? 'hide-content' : 'show-content'}`} onClick={() => this.props.addCourse(course)}>add</a>
-            <a className={`action  ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove course</a>
+            <a className={`action  ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove</a>
             </div>
             <AnimateHeight
           duration={ 300 }
